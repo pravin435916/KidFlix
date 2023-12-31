118ab6eeb54c954bb1b72421362b9ff6b4c36dc0
pravin nandankar <pravinnandankar03@gmail.com>
2024-01-01 03:44:45 +0530

    push

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <footer className="bg-gradient-to-t from-yellow-200 to-yellow-400 p-4 text-center">
       <p className="text-lg font-semibold">KidFlix - Your Kids' Movie Paradise</p>
       <p className="text-sm text-gray-700 mt-2">
-        © {new Date().getFullYear()} All rights reserved.
+        © {new Date().getFullYear()} All rights reserved. | Pravin Nandankar
       </p>
     </footer>
   );
